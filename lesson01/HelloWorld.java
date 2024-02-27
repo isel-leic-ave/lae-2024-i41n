82e848690d96323cc4d718d64367205f16e026f9
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2024-02-27 03:08:01 +0000

    Lesson 02: HelloWorld in Java and Kotlin and its mapping to the JVM type system.

---
+++ lesson01/HelloWorld.java
@@ -0,0 +1,9 @@
+record Xpto(int x) {}
+
+//public class Ypto { }
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("SLB in Java");
+    }
+}
---
+++ lesson01/HelloWorld.kt
@@ -0,0 +1,5 @@
+
+
+fun main() {
+    println("Glorioso SLB")
+}
